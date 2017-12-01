4f78bdf61059498d3b049d52298a31b199748ebf
Ye Min Naing <thargyi74@gmail.com>
2017-12-01 16:57:34 +0630

    Initial commit

---
+++ app/src/main/java/com/yeminnaing/righttransition/components/UserMenu.java
@@ -0,0 +1,33 @@
+package com.yeminnaing.righttransition.components;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by yeminnaing on 12/1/17.
+ */
+
+public class UserMenu extends LinearLayout {
+    public UserMenu(Context context) {
+        super(context);
+    }
+
+    public UserMenu(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public UserMenu(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public UserMenu(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+}
